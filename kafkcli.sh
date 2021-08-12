90ae592b035a440751664b8f43d7a8e441352e59
Akshay Mankumbare <mankumbareakshay444@gmail.com>
2021-08-12 18:18:29 +0100

    Version1

--- kafkcli.sh
+++ kafkcli.sh
@@ -1,9 +1,17 @@
 #!/bin/bash
+***************************************************************************
+#This script will only work from kafka broker on CDP.
+#If you want to run this on another nodes please make below changes to below variables :
+#SERVER
+#SECURITY_PROTOCOL
+#KAFKA_KEYTAB
+
+***************************************************************************
 
 export KAFKA_PROCESS_DIR=$(ls -1drt /var/run/cloudera-scm-agent/process/*KAFKA_BROKER | tail -1)
 export SERVER=$(grep -i listeners $KAFKA_PROCESS_DIR/kafka.properties | awk -F "/" '{print $3}' | sed 's/,//' )
 export SECURITY_PROTOCOL=$(grep -i listeners $KAFKA_PROCESS_DIR/kafka.properties | awk -F "=" '{print $2}' | awk -F ":" '{print $1}')
-
+export KAFKA_KEYTAB=$KAFKA_PROCESS_DIR/kafka.keytab
 
 exit_on_failure () {
 if [ $? -eq 0 ]
@@ -16,8 +24,7 @@ fi
 }
 
 # checking the security protocol to confirm if kerberos is enabled:
-if [ "$SECURITY_PROTOCOL" == "SASL_PLAINTEXT" ]  || [ "$SECURITY_PROTOCOL" == "PLAINTEXTSASL" ] ; then
-    echo "yes!"
+if [ "$SECURITY_PROTOCOL" == "SASL_PLAINTEXT" ]  || [ "$SECURITY_PROTOCOL" == "PLAINTEXTSASL" ] || [ "$SECURITY_PROTOCOL" == "SASL_SSL" ] ; then
 	while true; do
       	read -p $'\n\e[96mPlease confirm if you want to run as Kafka kerberos user (y/n) ? :\e[0m' yn
       	case $yn in
@@ -36,7 +43,7 @@ if [ "$SECURITY_PROTOCOL" == "SASL_PLAINTEXT" ]  || [ "$SECURITY_PROTOCOL" == "P
 	else
 		export PRINCIPAL=$(klist -kt $KAFKA_PROCESS_DIR/kafka.keytab | awk '{print $4}' | tail -1 )
 		echo "Running  : kinit -kt $KAFKA_PROCESS_DIR/kafka.keytab $PRINCIPAL "
-		kinit -kt $KAFKA_PROCESS_DIR/kafka.keytab $PRINCIPAL
+		kinit -kt $KAFKA_KEYTAB $PRINCIPAL
 		exit_on_failure
 	fi
 else
@@ -45,7 +52,23 @@ fi
 
 # This properties file will be used by all the commands :
 PROPERTIES_FILE=/tmp/client.properties
-echo "security.protocol=$SECURITY_PROTOCOL" > $PROPERTIES_FILE
+
+if [ "$SECURITY_PROTOCOL" == "SASL_SSL" ]  || [ "$SECURITY_PROTOCOL" == "SSL" ] ; then
+	read -p "Truststore Location: " TRUSTLOC
+	read -p "Truststore Password: " TRUSTPWD
+	read -p "Keystore Location (if client authentication is enable else press enter):" KEYLOC
+	read -p "Keystore Password (if client authentication is enable else press enter):" KEYPWD
+	read -p "Key Password (if client authentication is enable else press enter):" KPWD
+	echo "ssl.truststore.location=$TRUSTLOC" >> $PROPERTIES_FILE
+	echo "ssl.truststore.password=$TRUSTPWD" >> $PROPERTIES_FILE
+	echo "ssl.keystore.location=$KEYLOC" >> $PROPERTIES_FILE
+	echo "ssl.keystore.password=$KEYPWD" >> $PROPERTIES_FILE
+	echo "ssl.key.password=$KPWD"  >> $PROPERTIES_FILE
+	echo "security.protocol=$SECURITY_PROTOCOL" >> $PROPERTIES_FILE
+else
+echo "security.protocol=$SECURITY_PROTOCOL" >> $PROPERTIES_FILE
+fi
+
 
 # JAAS file template, you can update this is you want to use "useKeytab"
 if [ ! -f /tmp/client_jaas.conf ]
@@ -96,15 +119,24 @@ if ! [ -x "$(command -v kafka-console-consumer)" ]; then
 		   fi
 }
 
+check_kafka_consumer_group_command () {
+if ! [ -x "$(command -v kafka-consumer-groups)" ]; then
+  		   	echo -e "\e[31mError: kafka-consumer-groups script is not available.\e[0m"
+  		   	echo -e "\e[31mPlease install kafka client\e[0m"
+  		   	exit 1
+		   fi
+}
+
 options=(
 	"LIST ALL TOPIC" "DESCRIBE ALL THE TOPICS" 
 	"TOPICS WHOSE PARTITIONS ARE NOT AVAILABLE" 
 	"TOPICS WHICH DOES NOT MEET min.insync.replicas" 
 	"TOPICS WHICH DOES NOT MEET THE REPLICATION FACTOR OR REPLICAS ARE MISSING" 
 	"DESCRIBE A SPECIFIC TOPIC" 
-	"CREATE SPECIFIC TOPIC"
+	"CREATE A TOPIC"
 	"PRODUCE MESSAGES TO TOPIC"
 	"CONSUME MESSAGE FROM TOPIC"
+	"DESCRIBE CONSUMER GROUP"
 	"Quit"
 )
 
@@ -142,7 +174,7 @@ do
             echo "Running Command : kafka-topics --bootstrap-server $SERVER --topic $TOPIC --describe --command-config $PROPERTIES_FILE"
 			kafka-topics --bootstrap-server $SERVER --topic $TOPIC --describe --command-config $PROPERTIES_FILE
 			;;
-		"CREATE SPECIFIC TOPIC")
+		"CREATE A TOPIC")
 			check_kafka_topic_command
 			read -p "Enter Topic Name: " TOPIC
 			read -p "Enter Number Of Partitions: " PNO
@@ -173,6 +205,13 @@ do
 			read -p "Enter Additional Arguments to pass in the format --Argument value : " ADDTPCON
 			echo "Running Command : kafka-console-consumer --bootstrap-server $SERVER --consumer.config $PROPERTIES_FILE --topic $TOPIC $ADDTPCON"
 			kafka-console-consumer --bootstrap-server $SERVER --consumer.config $PROPERTIES_FILE --topic $TOPIC $ADDTPCON
+			;;
+		"DESCRIBE CONSUMER GROUP")
+			check_kafka_consumer_group_command
+			read -p "Enter Consumer Group: " CGRP
+			read -p "Enter Additional Arguments to pass in the format --Argument value : " ADDTPCON
+			echo "Running Command : kafka-consumer-groups --bootstrap-server $SERVER --describe  --group $CGRP --command-config /tmp/client.properties"
+			kafka-consumer-groups --bootstrap-server $SERVER --describe --command-config $PROPERTIES_FILE --group $CGRP  $ADDTPCON
 			;;
         "Quit")
             break
