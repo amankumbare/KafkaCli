5000fd534d3004c925a9e136da7f0970832f841b
Akshay Mankumbare <mankumbareakshay444@gmail.com>
2021-08-12 10:42:09 +0100

    kafkacli v1

---
+++ kafkcli.sh
@@ -0,0 +1,183 @@
+#!/bin/bash
+
+export KAFKA_PROCESS_DIR=$(ls -1drt /var/run/cloudera-scm-agent/process/*KAFKA_BROKER | tail -1)
+export SERVER=$(grep -i listeners $KAFKA_PROCESS_DIR/kafka.properties | awk -F "/" '{print $3}' | sed 's/,//' )
+export SECURITY_PROTOCOL=$(grep -i listeners $KAFKA_PROCESS_DIR/kafka.properties | awk -F "=" '{print $2}' | awk -F ":" '{print $1}')
+
+
+exit_on_failure () {
+if [ $? -eq 0 ]
+then
+    echo ""
+else
+	 echo "Please check the keytab and principal"
+	 exit 1;
+fi
+}
+
+# checking the security protocol to confirm if kerberos is enabled:
+if [ "$SECURITY_PROTOCOL" == "SASL_PLAINTEXT" ]  || [ "$SECURITY_PROTOCOL" == "PLAINTEXTSASL" ] ; then
+    echo "yes!"
+	while true; do
+      	read -p $'\n\e[96mPlease confirm if you want to run as Kafka kerberos user (y/n) ? :\e[0m' yn
+      	case $yn in
+         [Yy]* )  KAFKAUSER=yes ; break;;
+         [Nn]* )  KAFKAUSER=no ; break;;
+         * ) echo "Please answer yes or no.";;
+      	esac
+	done
+	
+	if  [ "$KAFKAUSER" == "no" ];then
+ 		read -p "Enter Complete path to Keytab: " Keytab
+ 		export PRINCIPAL=$(klist -kt $Keytab | awk '{print $4}' | tail -1 )
+ 		echo "Running : kinit -kt $Keytab $PRINCIPAL "
+ 		kinit -kt $Keytab $PRINCIPAL
+ 		exit_on_failure
+	else
+		export PRINCIPAL=$(klist -kt $KAFKA_PROCESS_DIR/kafka.keytab | awk '{print $4}' | tail -1 )
+		echo "Running  : kinit -kt $KAFKA_PROCESS_DIR/kafka.keytab $PRINCIPAL "
+		kinit -kt $KAFKA_PROCESS_DIR/kafka.keytab $PRINCIPAL
+		exit_on_failure
+	fi
+else
+ :
+fi
+
+# This properties file will be used by all the commands :
+PROPERTIES_FILE=/tmp/client.properties
+echo "security.protocol=$SECURITY_PROTOCOL" > $PROPERTIES_FILE
+
+# JAAS file template, you can update this is you want to use "useKeytab"
+if [ ! -f /tmp/client_jaas.conf ]
+then
+   cat <<EOF > /tmp/client_jaas.conf
+KafkaClient {
+com.sun.security.auth.module.Krb5LoginModule required
+useTicketCache=true
+renewTicket=true
+serviceName="kafka";
+};
+Client {
+com.sun.security.auth.module.Krb5LoginModule required
+useTicketCache=true
+renewTicket=true
+serviceName="zookeeper";
+};
+EOF
+fi
+
+echo "exporting JAAS file /tmp/client_jaas.conf"
+export KAFKA_OPTS="-Djava.security.auth.login.config=/tmp/client_jaas.conf"
+
+
+PS3='Please enter your choice: '
+# Checking if the commands are available on the node:
+check_kafka_topic_command () {
+if ! [ -x "$(command -v kafka-topics)" ]; then
+  		   	echo -e "\e[31mError: kafka-topics script is not available.\e[0m"
+  		   	echo -e "\e[31mPlease install kafka client\e[0m"
+  		   	exit 1
+		   fi
+}
+
+check_kafka_producer_command () {
+if ! [ -x "$(command -v kafka-console-producer)" ]; then
+  		   	echo -e "\e[31mError: kafka-console-producer script is not available.\e[0m"
+  		   	echo -e "\e[31mPlease install kafka client\e[0m"
+  		   	exit 1
+		   fi
+}
+
+check_kafka_consumer_command () {
+if ! [ -x "$(command -v kafka-console-consumer)" ]; then
+  		   	echo -e "\e[31mError: kafka-console-producer script is not available.\e[0m"
+  		   	echo -e "\e[31mPlease install kafka client\e[0m"
+  		   	exit 1
+		   fi
+}
+
+options=(
+	"LIST ALL TOPIC" "DESCRIBE ALL THE TOPICS" 
+	"TOPICS WHOSE PARTITIONS ARE NOT AVAILABLE" 
+	"TOPICS WHICH DOES NOT MEET min.insync.replicas" 
+	"TOPICS WHICH DOES NOT MEET THE REPLICATION FACTOR OR REPLICAS ARE MISSING" 
+	"DESCRIBE A SPECIFIC TOPIC" 
+	"CREATE SPECIFIC TOPIC"
+	"PRODUCE MESSAGES TO TOPIC"
+	"CONSUME MESSAGE FROM TOPIC"
+	"Quit"
+)
+
+select opt in "${options[@]}"
+do
+    case $opt in
+        "LIST ALL TOPIC")
+           check_kafka_topic_command
+		   echo "Running Command : kafka-topics --bootstrap-server $SERVER --list --command-config $PROPERTIES_FILE"
+		   kafka-topics --bootstrap-server $SERVER --list --command-config $PROPERTIES_FILE
+            ;;
+        "DESCRIBE ALL THE TOPICS")
+            check_kafka_topic_command
+            echo "Running Command : kafka-topics --bootstrap-server $SERVER --describe --command-config $PROPERTIES_FILE"
+			kafka-topics --bootstrap-server $SERVER --describe --command-config $PROPERTIES_FILE
+            ;;
+        "TOPICS WHOSE PARTITIONS ARE NOT AVAILABLE")
+         	check_kafka_topic_command
+            echo "Running Command : kafka-topics --bootstrap-server $SERVER --describe --command-config $PROPERTIES_FILE --unavailable-partitions "
+			kafka-topics --bootstrap-server $SERVER --describe --command-config $PROPERTIES_FILE --unavailable-partitions
+            ;;
+        "TOPICS WHICH DOES NOT MEET min.insync.replicas")
+        	check_kafka_topic_command
+        	echo "Running Command : kafka-topics --bootstrap-server $SERVER --describe --command-config $PROPERTIES_FILE --under-min-isr-partitions "
+			kafka-topics --bootstrap-server $SERVER --describe --command-config $PROPERTIES_FILE --under-min-isr-partitions
+			;;
+		"TOPICS WHICH DOES NOT MEET THE REPLICATION FACTOR OR REPLICAS ARE MISSING")
+			check_kafka_topic_command
+			echo "Running Command : kafka-topics --bootstrap-server $SERVER --describe --command-config $PROPERTIES_FILE --under-replicated-partitions"
+			kafka-topics --bootstrap-server $SERVER --describe --command-config $PROPERTIES_FILE --under-replicated-partitions
+			;;
+		"DESCRIBE A SPECIFIC TOPIC")
+			check_kafka_topic_command
+			read -p "Enter Topic Name: " TOPIC
+            echo "Running Command : kafka-topics --bootstrap-server $SERVER --topic $TOPIC --describe --command-config $PROPERTIES_FILE"
+			kafka-topics --bootstrap-server $SERVER --topic $TOPIC --describe --command-config $PROPERTIES_FILE
+			;;
+		"CREATE SPECIFIC TOPIC")
+			check_kafka_topic_command
+			read -p "Enter Topic Name: " TOPIC
+			read -p "Enter Number Of Partitions: " PNO
+			read -p "Enter The Number Of Replicas: " RF
+			read -p "Enter Additional Arguments to pass in the format --Argument value: " ADDTPCON
+			echo "Running Command : kafka-topics --bootstrap-server $SERVER --topic $TOPIC --create --partitions $PNO --replication-factor $RF --command-config $PROPERTIES_FILE $ADDTPCON"
+			kafka-topics --bootstrap-server $SERVER --topic $TOPIC --create --partitions $PNO --replication-factor $RF --command-config $PROPERTIES_FILE $ADDTPCON
+			;;
+		"PRODUCE MESSAGES TO TOPIC")
+			check_kafka_producer_command
+			read -p "Enter Topic Name: " TOPIC
+			read -p "How Many Messages to produce " MSGNU
+			read -p "Enter Additional Arguments to pass in the format --Argument value : " ADDTPCON
+			if [ ! -f  $( eval echo /tmp/$MSGNU.txt ) ]
+			then
+			for a in $( eval echo {1..$MSGNU} ); do echo "This is Message no $a"; done > /tmp/$MSGNU.txt
+			else
+			echo > /tmp/$MSGNU.txt
+			for a in $( eval echo {1..$MSGNU} ); do echo "This is Message no $a"; done > /tmp/$MSGNU.txt
+			fi
+			echo "Running Command : kafka-console-producer --bootstrap-server $SERVER --producer.config $PROPERTIES_FILE --topic $TOPIC $ADDTPCON < /tmp/$MSGNU.txt"
+			kafka-console-producer --bootstrap-server $SERVER --producer.config $PROPERTIES_FILE --topic $TOPIC $ADDTPCON < /tmp/$MSGNU.txt
+			echo "Delete the file /tmp/$MSGNU.txt"
+			;;
+		"CONSUME MESSAGE FROM TOPIC")
+			check_kafka_consumer_command
+			read -p "Enter Topic Name: " TOPIC
+			read -p "Enter Additional Arguments to pass in the format --Argument value : " ADDTPCON
+			echo "Running Command : kafka-console-consumer --bootstrap-server $SERVER --consumer.config $PROPERTIES_FILE --topic $TOPIC $ADDTPCON"
+			kafka-console-consumer --bootstrap-server $SERVER --consumer.config $PROPERTIES_FILE --topic $TOPIC $ADDTPCON
+			;;
+        "Quit")
+            break
+            ;;
+        *) echo "invalid option $REPLY";;
+    esac
+done
+
